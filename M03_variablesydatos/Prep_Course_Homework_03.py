02a050231688cf6116a6f9b5bad2f0d62d9f4b2e
nina ro <160553312+ninaD2024@users.noreply.github.com>
2024-03-22 01:22:36 -0300

    Update Prep_Course_Homework_03.py

--- M03_variablesydatos/Prep_Course_Homework_03.py
+++ M03_variablesydatos/Prep_Course_Homework_03.py
@@ -7,13 +7,14 @@
 
 # In[7]:
 
-
+num= 3
+print (num)
 
 
 # 2) Imprimir el tipo de dato de la constante 8.5
 
 # In[3]:
-
+type(8.5)
 
 
 
@@ -22,7 +23,7 @@
 
 # In[8]:
 
-
+type(num)
 
 
 
@@ -30,13 +31,13 @@
 
 # In[2]:
 
-
+nombre= "Nina"
 
 
 # 5) Crear una variable que contenga un número complejo
 
 # In[3]:
-
+num_compl= 8+ 8j
 
 
 
@@ -44,7 +45,7 @@
 # 6) Mostrar el tipo de dato de la variable crada en el punto 5
 
 # In[4]:
-
+type(num_complejo)
 
 
 
@@ -61,14 +62,17 @@ pi = 3.1416
 
 # In[3]:
 
-
+var1= "true"
+var2= True
+No son lo mismo ya que, var1 es una cadena de texto y var2 es un booleano
 
 
 
 # 9) Imprimir el tipo de dato correspondientes a las variables creadas en el punto 8
 
 # In[5]:
-
+type(var1)
+type(var2)
 
 
 
@@ -77,7 +81,7 @@ pi = 3.1416
 
 # In[1]:
 
-
+print (" La var1 es de tipo ", type(var1), " y la var2 es de tipo ", type(var2))
 
 
 
@@ -85,7 +89,9 @@ pi = 3.1416
 
 # In[2]:
 
-
+s1= 5 + 8j
+s2= 8 + 7j
+print (s1 + s2)
 
 
 
@@ -93,15 +99,17 @@ pi = 3.1416
 
 # In[4]:
 
-
+r= 6
+c=3 + 4j
+suma=(r + c)
+print (suma)
 
 
 
 # 13) Realizar una operación de multiplicación
 
 # In[5]:
-
-
+print (4*9)
 
 
 
@@ -109,21 +117,24 @@ pi = 3.1416
 
 # In[6]:
 
-
+y=2
+x=8
+print (y**x)
 
 
 # 15) Obtener el cociente de la división de 27 entre 4 en una variable y luego mostrarla
 
 # In[8]:
 
-
+div= (27/4)
+print(div)
 
 
 
 # 16) De la división anterior solamente mostrar la parte entera
 
 # In[9]:
-
+print (27//4)
 
 
 
@@ -132,7 +143,7 @@ pi = 3.1416
 
 # In[1]:
 
-
+print(27%4)
 
 
 
@@ -140,7 +151,7 @@ pi = 3.1416
 
 # In[2]:
 
-
+6*4+3
 
 
 
@@ -148,14 +159,16 @@ pi = 3.1416
 
 # In[3]:
 
-
-
-
+pal1= "Soy"
+pal2= "Nina"
+print (pal1 + pal2)
 
 # 20) Evaluar si "2" es igual a 2. ¿Por qué ocurre eso?
 
 # In[4]:
+"2"==2
 
+Ocurre porque "2" es un string y 2 es un int por lo tant es False
 
 
 
@@ -165,14 +178,14 @@ pi = 3.1416
 # In[11]:
 
 
-
+2==int("2")
 
 
 # 22) ¿Por qué arroja error el siguiente cambio de tipo de datos? a = float('3,8')
 
 # In[12]:
 
-
+Porque ("3.8")no se puede convertir a float
 
 
 
@@ -180,14 +193,16 @@ pi = 3.1416
 
 # In[15]:
 
-
+var_1= 3
+var_1 -= 1
+print(var_1)
 
 
 
 # 24) Realizar la operacion 1 << 2 ¿Por qué da ese resultado? ¿Qué es el sistema de numeración binario?
 
 # In[29]:
-
+1<<2
 
 
 
@@ -196,7 +211,8 @@ pi = 3.1416
 
 # In[23]:
 
-
+Son diferentes elementos, uno int y el otro str.
+Si fueran del mismo tipo no daría error
 
 
 
@@ -205,5 +221,6 @@ pi = 3.1416
 
 # In[30]:
 
-
-
+pal1="hola"
+pal=2
+print (pal1 *pal2)
